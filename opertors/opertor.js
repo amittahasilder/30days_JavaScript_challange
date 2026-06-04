2015f81130279b43570f7ed70e785bcf1d65611f
amittahasilder <amittahasilder@gmail.com>
2026-06-03 18:53:51 -0700

    first commit

---
+++ opertors/opertor.js
@@ -0,0 +1,16 @@
+let age = 20;
+let hasID = true;
+
+console.log(age >= 22 && hasID);
+
+
+
+let marks = 85;
+
+if (marks >= 80) {
+  console.log("A+");
+} else if (marks >= 70) {
+  console.log("A");
+} else {
+  console.log("Fail");
+}
