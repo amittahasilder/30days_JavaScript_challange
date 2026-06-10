9d7b682e8e406219a1982e25cc80b143af5bb456
amittahasilder <amittahasilder@gmail.com>
2026-06-09 18:31:33 -0700

    your message

---
+++ function/fun2.js
@@ -0,0 +1,23 @@
+function add(a,b){
+    return a+b;
+}
+
+add(5,8);
+
+
+// nameed function
+
+function amit (){
+    return "hello";
+}
+
+
+// Anonymous Function
+
+const  greet = function(){
+    return "Hi there!";
+};
+
+console.log(greet());
+
+// . Function Expression
