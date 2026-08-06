e2fbb830a00bbfe9cb676d5adc7973426765b4f0
amittahasilder <amittahasilder@gmail.com>
2026-08-06 09:51:07 -0700

    your message

---
+++ Asynch/problem/problem.js
@@ -0,0 +1,21 @@
+var timeLimit = function(fn, t) {
+
+    return async function(...args) {
+
+        return Promise.race([
+
+            fn(...args),
+
+            new Promise((_, reject)=>{
+
+                setTimeout(()=>{
+                    reject("Time Limit Exceeded");
+                },t);
+
+            })
+
+        ]);
+
+    }
+
+};
